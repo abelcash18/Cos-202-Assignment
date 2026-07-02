8dd59d8cea097397649d1a1d107fd1ba3e5d1b99
Joseph Abel <josephabel540@gmail.com>
2026-07-02 14:08:42 +0100

    assign

---
+++ Lab 1/SumCalculator.java
@@ -0,0 +1,15 @@
+public class SumCalculator {
+    public static int computeSum(int n) {
+        int total = 0;
+        // i will go from 1 up to n
+        for (int i = 1; i <= n; i++) {
+            total += i;  // <- Breakpoint placed here
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        // Example execution
+        computeSum(5);
+    }
+}
