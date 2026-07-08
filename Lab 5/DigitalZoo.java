82131f1bab8d506a18ebd4b11652f3711c588a07
Joseph Abel <josephabel540@gmail.com>
2026-07-08 18:42:39 +0100

    edu

--- Lab 5/DigitalZoo.java
+++ Lab 5/DigitalZoo.java
@@ -1,3 +1,11 @@
+// Name: Joseph Abel Olayinka
+// Matric no:  EDU2509276
+// Faculty: Education
+// Department: Curriculum and Instructional Technology
+// Course area: Computer Science Education
+ // Course: Cos 202
+ // Level:200 Direct-Entry
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,28 +14,24 @@ class Animal {
         System.out.println("Some generic animal sound");
     }
 }
-
 class Dog extends Animal {
     @Override
     void makeSound() {
         System.out.println("Woof");
     }
 }
-
 class Cat extends Animal {
     @Override
     void makeSound() {
         System.out.println("Meow");
     }
 }
-
 class Bird extends Animal {
     @Override
     void makeSound() {
         System.out.println("chirp");
     }
 }
-
 public class DigitalZoo {
     public static void main(String[] args) {
         List<Animal> zoo = new ArrayList<>();
